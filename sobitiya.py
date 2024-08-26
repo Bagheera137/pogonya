8d19ea708da9a32347501efc27a84d9236d285a0
Boss <kosevaa@mail.ru>
2024-08-26 15:08:58 +0300

    tyui

--- sobitiya.py
+++ sobitiya.py
@@ -2,7 +2,8 @@ import wrap
 
 wrap.world.create_world(500,500)
 pacman=wrap.sprite.add("pacman", 300,200,"player2")
-blue=wrap.sprite.add("pacman",100,400,"enemy_ill_blue1")
+blue=wrap.sprite.add("pacman",100,400,"enemy_ill_blue1",False)
+
 
 @wrap.on_key_down(wrap.K_RIGHT,wrap.K_LEFT)
 def sdvig(key):
@@ -13,11 +14,25 @@ def sdvig(key):
         a = wrap.sprite.get_angle(pacman)
         wrap.sprite.set_angle(pacman, a-20)
 
+
 @wrap.on_key_always(wrap.K_UP)
 def sdvig():
     wrap.sprite.move_at_angle_dir(pacman,20)
 @wrap.on_mouse_move
 def move(pos_x,pos_y):
     wrap.sprite.move_to(blue,pos_x,pos_y)
+@wrap.on_mouse_down
+def click():
+    wrap.sprite.show(blue)
+@wrap.on_mouse_up
+def click():
+    wrap.sprite.hide(blue)
+
+@wrap.always(20)
+def always():
+    bx = wrap.sprite.get_x(blue)
+    by = wrap.sprite.get_y(blue)
+    wrap.sprite.set_angle_to_point(pacman, bx, by)
+
 
 
