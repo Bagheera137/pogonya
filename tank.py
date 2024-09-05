cc9ade2a88de3fc8e057c01f08f6f1a25a2b6f6b
Boss <kosevaa@mail.ru>
2024-09-05 20:41:02 +0300

    tyui

--- tank.py
+++ tank.py
@@ -1,10 +1,11 @@
 import random
 
 import wrap
-
+a=1
 wrap.world.create_world(400, 600)
 tank1=wrap.sprite.add("battle_city_tanks",200,100,"tank_enemy_size1_green1")
-pric = wrap.sprite.add("pacman", 300, 200, "player2")
+pric = wrap.sprite.add("pacman", 300, 200, "player2",False)
+wrap.sprite.set_size(pric,15,15)
 wrap.sprite.set_angle(pric, 0)
 @wrap.always(2000)
 def always():
@@ -21,4 +22,17 @@ def move(pos_x,pos_y):
     wrap.sprite.move_to(pric,pos_x,pos_y)
 @wrap.on_key_down(wrap.K_UP)
 def size():
-    wrap.sprite.set_size(pric,15,15)
+    global a
+    h=wrap.sprite.get_height(pric)
+    w=wrap.sprite.get_width(pric)
+    if a==1:
+        wrap.sprite.show(pric)
+    if a==2 or a==3:
+        wrap.sprite.set_size(pric,h+20,w+20)
+    if a==4:
+        wrap.sprite.hide(pric)
+        wrap.sprite.set_size(pric, 15, 15)
+        a=0
+
+    a = a + 1
+    print(h,w)
