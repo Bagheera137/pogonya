588dbec845721af6ab6eb273538c8b4c6530db94
Boss <kosevaa@mail.ru>
2024-09-15 16:02:51 +0300

    tyui

--- supergeroy.py
+++ supergeroy.py
@@ -16,10 +16,23 @@ def move(keys):
         wrap.sprite.set_reverse_x(mario,True)
         if wrap.sprite.get_left(mario)<0:
             wrap.sprite.move_left_to(mario,0)
+
+probel=False
 @wrap.on_key_down(wrap.K_SPACE)
 def fly():
+    global probel
     wrap.sprite.set_costume(mario,"swim6")
+    probel=True
 
 @wrap.always
-def move(pos_x,pos_y):
-    wrap.sprite.move_at_angle_point(mario,pos_x,pos_y,10)
+def move(pos_x, pos_y,keys):
+    print(1)
+    if probel:
+        wrap.sprite.move_at_angle_point(mario, pos_x, pos_y, 10)
+@wrap.on_key_down(wrap.K_DOWN)
+def down():
+    global probel
+    probel=False
+    a=wrap.sprite.get_x(mario)
+    wrap.sprite.move_to(mario,a,550)
+    wrap.sprite.set_costume(mario, "stand")
